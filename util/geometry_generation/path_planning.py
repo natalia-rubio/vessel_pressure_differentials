8efbec175c75dcebea5d26f48ba7b947a7aa227b
natalia-rubio <natalialeonie72@gmail.com>
2023-12-29 21:17:17 -0800

    path and radius planning for simple geometries

---
+++ util/geometry_generation/path_planning.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_path(length, curvature, elem_length=1):
+    """
+    Returns a path of a given length and curvature
+    curvature varies between -90 and 90 degrees
+    """
+    num_pts = int(length / elem_length)
+    angles = np.linspace(0, curvature, num_pts)
+    path = [[0, 0, 0],]
+
+    for i in range(num_pts):
+        x = path[-1][0] + elem_length * np.cos(angles[i]*np.pi/180)
+        y = path[-1][1] + elem_length * np.sin(angles[i]*np.pi/180)
+        z = 0
+        path.append([x, y, z])
+
+    return path
+
+def plot_2D_path(path):
+    """
+    Plots a path
+    """
+    plt.clf()
+    path = np.array(path)
+    plt.plot(path[:, 0], path[:, 1])
+    plt.show()
+    return
+
+if __name__ == '__main__':
+    path = get_path(length=10, curvature=90)
+    plot_2D_path(path)
---
+++ util/geometry_generation/radius_planning.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+def get_radii(length, inlet_radius, outlet_radius, stenosis_dict, elem_length=1):
+    """
+    Returns a list of radii
+    """
+    num_pts = int(length / elem_length)
+    base_radii = np.linspace(inlet_radius, outlet_radius, num_pts)
+
+    pts = np.linspace(0, num_pts, num_pts)
+    stenosis_pdf = norm.pdf(pts, loc = num_pts*stenosis_dict["location"], scale = num_pts*stenosis_dict["spread"])
+    stenosis_multiplier = 1 + stenosis_dict["magnitude"] * stenosis_pdf/np.max(stenosis_pdf)
+    #print(stenosis_multiplier)
+    return list(base_radii * stenosis_multiplier)
+
+def plot_radii(radii):
+    """
+    Plots radius along straight path
+    """
+    plt.clf()
+    plt.plot(np.array(radii), "b")
+    plt.plot(-1*np.array(radii), "b")
+    plt.show()
+    return
+
+if __name__ == '__main__':
+    stenosis_dict = {"magnitude": 0.5, 
+                    "spread": 0.1,
+                    "location": 0.5}
+    radii = get_radii(length=10, inlet_radius=1, outlet_radius=2, stenosis_dict=stenosis_dict)
+    plot_radii(radii)
